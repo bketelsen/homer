23303b522c76ecddb303bc319708649458447226
Brian Ketelsen <bketelsen@gmail.com>
2024-10-11 21:20:32 -0400

    fix: cockpit fonts for zfs

--- build_files/fetch-install.sh
+++ build_files/fetch-install.sh
@@ -15,14 +15,16 @@ git clone https://github.com/45drives/cockpit-zfs-manager.git
 cp -r cockpit-zfs-manager/zfs /usr/share/cockpit
 rm -rf cockpit-zfs-manager
 
-mkdir -p /usr/share/cockpit/base1/fonts/
-wget -O /usr/share/cockpit/base1/fonts/fontawesome.woff https://github.com/h5p/font-awesome/raw/master/fontawesome-webfont.woff 
-wget -O /usr/share/cockpit/base1/fonts/glyphicons.woff https://github.com/twbs/bootstrap-sass/raw/master/assets/fonts/bootstrap/glyphicons-halflings-regular.woff 
-wget -O /usr/share/cockpit/base1/fonts/patternfly.woff https://github.com/patternfly/patternfly-sass/raw/master/assets/fonts/patternfly/PatternFlyIcons-webfont.woff
-wget -P /usr/share/cockpit/static/fonts https://github.com/braintree/braintree_slim_example/raw/main/static/fonts/open-sans/OpenSans-Semibold-webfont.woff
-mkdir -p /usr/share/cockpit/zfs/assets/fonts/RedHatDisplay/
-cp -v /usr/share/cockpit/static/fonts/*Display*.woff2 /usr/share/cockpit/zfs/assets/fonts/RedHatDisplay/
-for f in /usr/share/cockpit/zfs/assets/fonts/RedHatDisplay/*; do mv -v -- "$f" "${f%.woff2}.woff"; done
-mkdir -p /usr/share/cockpit/zfs/assets/fonts/RedHatText/
-cp -v /usr/share/cockpit/static/fonts/*Text*.woff2 /usr/share/cockpit/zfs/assets/fonts/RedHatText/
-for f in /usr/share/cockpit/zfs/assets/fonts/RedHatText/*; do mv -v -- "$f" "${f%.woff2}.woff"; done
+echo Fixing cockpit fonts
+
+mkdir -p /usr/share/cockpit/base1/fonts
+
+curl -sSL https://scripts.45drives.com/cockpit_font_fix/fonts/fontawesome.woff -o /usr/share/cockpit/base1/fonts/fontawesome.woff
+curl -sSL https://scripts.45drives.com/cockpit_font_fix/fonts/glyphicons.woff -o /usr/share/cockpit/base1/fonts/glyphicons.woff
+curl -sSL https://scripts.45drives.com/cockpit_font_fix/fonts/patternfly.woff -o /usr/share/cockpit/base1/fonts/patternfly.woff
+
+mkdir -p /usr/share/cockpit/static/fonts
+
+curl -sSL https://scripts.45drives.com/cockpit_font_fix/fonts/OpenSans-Semibold-webfont.woff -o /usr/share/cockpit/static/fonts/OpenSans-Semibold-webfont.woff
+
+echo Done
