035246a0689dff70dd740e7fe3a91765970a2339
Brian Ketelsen <bketelsen@gmail.com>
2025-01-09 13:56:52 -0500

    chore: remove cockpit zfs

--- build_files/fetch-install.sh
+++ build_files/fetch-install.sh
@@ -12,20 +12,5 @@ echo 'eval "$(starship init bash)"' >> /etc/bashrc
 # cockpit and extensions
 rpm-ostree install cockpit-files cockpit-machines cockpit-networkmanager cockpit-ostree cockpit-podman cockpit-selinux cockpit-storaged cockpit-system 
 
-git clone https://github.com/45drives/cockpit-zfs-manager.git
-cp -r cockpit-zfs-manager/zfs /usr/share/cockpit
-rm -rf cockpit-zfs-manager
-
-echo Fixing cockpit fonts
-
-mkdir -p /usr/share/cockpit/base1/fonts
-
-curl -sSL https://scripts.45drives.com/cockpit_font_fix/fonts/fontawesome.woff -o /usr/share/cockpit/base1/fonts/fontawesome.woff
-curl -sSL https://scripts.45drives.com/cockpit_font_fix/fonts/glyphicons.woff -o /usr/share/cockpit/base1/fonts/glyphicons.woff
-curl -sSL https://scripts.45drives.com/cockpit_font_fix/fonts/patternfly.woff -o /usr/share/cockpit/base1/fonts/patternfly.woff
-
-mkdir -p /usr/share/cockpit/static/fonts
-
-curl -sSL https://scripts.45drives.com/cockpit_font_fix/fonts/OpenSans-Semibold-webfont.woff -o /usr/share/cockpit/static/fonts/OpenSans-Semibold-webfont.woff
 
 echo Done
